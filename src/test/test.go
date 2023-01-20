554ad2b5a6009540d46b699dfd615942fe17b86a
austinr <richard.david.austin@gmail.com>
2023-01-20 18:24:43 +0000

    Add facility to kill ffmpeg process after a random time (within a max/min range).
    Add autoRestart flag to determine whether ffmpeg processes are restarted after auto kill.

--- src/test/test.go
+++ src/test/test.go
@@ -2,16 +2,23 @@ package main
 
 import (
 	"fmt"
+	log "github.com/sirupsen/logrus"
+	"math/rand"
 	"os"
 	"os/exec"
 	"os/signal"
+	"syscall"
 	"time"
 )
 
 func main() {
+	rand.Seed(time.Now().UnixNano())
+	minDelay := 3
+	maxDelay := 200
 
-	maxInstances := 12
-
+	maxInstances := 10
+	autoKill := true
+	autoRestart := false
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt)
 
@@ -22,13 +29,28 @@ func main() {
 				for {
 					cmdStr := fmt.Sprintf("/usr/bin/ffmpeg -y -f mp4 -i https://192.168.0.29/h/stream?suuid=stream%d -c copy -f mp4 test_file_%d_%d.mp4f", stream, stream, instance)
 					cmd := exec.Command("bash", "-c", cmdStr)
-					_, err := cmd.Output()
-					if err != nil {
-						fmt.Printf("%s", err.Error())
-					} else {
-						fmt.Printf("%s", "OK")
+					cmd.Start()
+					pid := cmd.Process.Pid
+
+					if autoKill {
+						go func() {
+							delay := rand.Intn(maxDelay-minDelay+1) + minDelay
+							time.Sleep(time.Second * time.Duration(delay))
+							syscall.Kill(pid, syscall.SIGINT)
+						}()
 					}
-					time.Sleep(time.Second)
+					err := cmd.Wait()
+					ee := err.(*exec.ExitError)
+					if ee != nil && ee.ExitCode() != 255 {
+						log.Errorf("ffmpeg :- %s, %s", string(ee.Stderr), ee.Error())
+					} else {
+						log.Errorf("ffmpeg :- :- %s", err.Error())
+					}
+					if autoRestart {
+						time.Sleep(time.Second)
+					} else {
+						break
+					}
 				}
 			}(stream, instance)
 		}
