92ef0928f52b3bf70b3c41a74e69dae3b7a0b4db
richard-austin <richard.david.austin@gm>
2025-04-16 16:40:11 +0100

    * Found a way to get audio and video in snc while recording from separate audio and video streams so ditching the mp4f stream for recording idea. It gives a lot of warnings when adding wallclock timestamps to the audio, but the result seems to work well enough.

--- src/gop-cache.go
+++ src/gop-cache.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	log "github.com/sirupsen/logrus"
 	"sync"
 )
 
@@ -73,28 +72,6 @@ func (g *GopCache) AudioInput(p Packet) (err error) {
 	} else {
 		err = fmt.Errorf("audio GOP cache is full")
 	}
-	return
-}
-
-func (g *GopCache) RecordingInput(p Packet) (err error) {
-	err = nil
-	if !g.GopCacheUsed {
-		return
-	}
-	g.mutex.Lock()
-	defer g.mutex.Unlock()
-
-	if (p.isMoof() || g.inputIndex == 0) && p.isFmp4KeyFrame() {
-		g.inputIndex = 0
-	}
-	if g.inputIndex < g.cacheLength {
-		g.Cache[g.inputIndex] = p
-		g.inputIndex++
-	} else {
-		err = fmt.Errorf("GOP Cache is full")
-	}
-
-	log.Infof("fmp4 gop cache index = %d", g.inputIndex)
 	return
 }
 
